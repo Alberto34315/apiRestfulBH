d9ec4767857e91503aac1d49bc62cf7cee810ff3
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2021-02-18 19:15:57 +0100

    first commit

--- src/main/java/com/alberto/apirestfulservice/repositories/exerciseRepository.java
+++ src/main/java/com/alberto/apirestfulservice/repositories/exerciseRepository.java
@@ -14,10 +14,10 @@ import org.springframework.data.jpa.repository.Query;
 @Repository
 public interface exerciseRepository extends JpaRepository<exercise, Long> {
 
-	@Query(value="SELECT * FROM exercise AS i WHERE i.nameExercise LIKE %?1%", nativeQuery=true)
+	@Query(value="SELECT * FROM exercise AS i WHERE i.name_exercise LIKE %?1%", nativeQuery=true)
     public List<exercise> getByTitle(String title);
     
-	@Query(value="SELECT * FROM exercise AS i WHERE i.nameExercise LIKE %?1% AND i.iduser=?2", nativeQuery=true)
+	@Query(value="SELECT * FROM exercise AS i WHERE i.name_exercise LIKE %?1% AND i.iduser=?2", nativeQuery=true)
     public List<exercise> getByTitleFromUser(String title,Long code);
     
     @Query(value="SELECT * FROM exercise AS i WHERE i.iduser=?1", nativeQuery=true)
