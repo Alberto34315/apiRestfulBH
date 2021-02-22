1861d58c1f86713008b584510446f486824bf7e9
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2021-02-22 12:20:33 +0100

    make it better

--- src/main/java/com/alberto/apirestfulservice/controllers/trainingServiceController.java
+++ src/main/java/com/alberto/apirestfulservice/controllers/trainingServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.alberto.apirestfulservice.services.exerciseService;
 import com.alberto.apirestfulservice.services.trainingService;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/training")
 public class trainingServiceController {
 
